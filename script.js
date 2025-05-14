912b4a82854d172864ecebd5bf43110fcd3a2b2d
upasanaksoni <upasanakumarikumari@gmail.com>
2025-05-14 22:49:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+
+window.addEventListener('load', checkInternetConnection);
+
+function checkInternetConnection() {
+    const statusText = document.getElementById('statusText');
+    const ipAddressText = document.getElementById('ipAddressText');
+    const networkStrengthText = document.getElementById('networkStrengthText');
+
+    statusText.textContent = 'Checking...';
+
+    if (navigator.onLine) {
+        fetch('https://api.ipify.org?format=json')
+            .then((response) => response.json())
+            .then((data) => {
+                ipAddressText.textContent = data.ip;
+                statusText.textContent = 'Connected';
+
+                const connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection;
+                const networkStrength = connection ? connection.downlink + ' Mbps' : 'Unknown';
+
+                networkStrengthText.textContent = networkStrength;
+            })
+            .catch(() => {
+                statusText.textContent = 'Disconnected';
+                ipAddressText.textContent = '-';
+                networkStrengthText.textContent = '-';
+            });
+    } else {
+        statusText.textContent = 'Disconnected';
+        ipAddressText.textContent = '-';
+        networkStrengthText.textContent = '-';
+    }
+}
+
